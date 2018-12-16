a7573dd29610a45cc275dfc958518842404a0aa3
Clayton Wong <clayton.wong@ookla.com>
2018-12-15 20:49:37 -0800

    minor edits

--- 2018-12-03_overlapping_claims/main.cpp
+++ 2018-12-03_overlapping_claims/main.cpp
@@ -27,30 +27,26 @@ class Solution
     FabricCells visited, duplicate;
     unordered_map< int, FabricCells > claims;
 
-    void traverse( const int id, const int col, const int row, const int width, const int height )
-    {
-        claims[ id ] = {};
-        for( int i{ row }; i < row + height; ++i )
-            for( int j{ col }; j < col + width; claims[ id ].insert( {i,j} ), ++j )
-                if( ! visited.insert( {i,j} ).second )
-                    duplicate.insert( {i,j} );
-    }
-
 public:
 
     PII getOverlappingClaims( const string& input, int id=0, int col=0, int row=0, int width=0, int height=0, PII ans={} )
-    {                                                              //   1      2     3      4       5
-        regex pattern("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$"); // #(id) @ (col),(row): (width)x(height)
+    {                                                                //   1      2     3      4       5
+        regex pattern( "^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$" ); // #(id) @ (col),(row): (width)x(height)
         istringstream stream{ input };
         for( string line; getline( stream, line ); )
         {
             smatch group;
-            if (regex_match(line, group, pattern) && group.size() == 6)
+            if( regex_match( line, group, pattern ) && group.size() == 6 )
             {
                 stringstream stream;
-                stream << group[1] << ' ' << group[2] << ' ' << group[3] << ' ' << group[4] << ' ' << group[5];
+                stream << group[ 1 ] << ' ' << group[ 2 ] << ' ' << group[ 3 ] << ' ' << group[ 4 ] << ' ' << group[ 5 ];
                 stream >> id >> col >> row >> width >> height;
-                traverse( id, col, row, width, height );
+
+                claims[ id ] = {};
+                for( int i{ row }; i < row + height; ++i )
+                    for( int j{ col }; j < col + width; claims[ id ].insert( {i,j} ), ++j )
+                        if( ! visited.insert( {i,j} ).second )
+                            duplicate.insert( {i,j} );
             }
         }
         ans.first = static_cast< int >( duplicate.size() );
