66838800ea2a7a94af073dfc7506329dd9493dfb
Clayton Wong <clayton.wong@ookla.com>
2018-12-16 09:55:45 -0800

    2018-12-10_the_stars_align 'hi' is working

---
+++ 2018-12-10_the_stars_align/main.cpp
@@ -0,0 +1,109 @@
+//
+// Created by Clayton Wong on 2018-12-16.
+//
+
+#include "input.hpp"
+#include <iostream>
+#include <vector>
+#include <regex>
+#include <sstream>
+#include <unordered_set>
+
+
+struct Position{ int row{ 0 }, col{ 0 }; };
+struct Velocity{ int row{ 0 }, col{ 0 }; };
+struct Light{ Position pos; Velocity speed; };
+
+
+using namespace std;
+using Lights = vector< Light >;
+using Aligned = vector< int >; // unique alignments of row/col per tick
+using Unique = unordered_set< int >;
+
+
+struct Answer
+{
+    Lights lights;
+    int tick{ 0 }, minRow{ 0 }, maxRow{ 0 }, minCol{ 0 }, maxCol{ 0 };
+};
+
+
+class Solution
+{
+public:
+    Answer getMaxAligned( const string& input, int row=0, int col=0,
+        int minUnaligned=numeric_limits<int>::max(), int minTick=0,
+        int minRow=0, int maxRow=0, int minCol=0, int maxCol=0,
+        Lights lights={}, Lights alignedLights={}, Aligned unaligned={},
+        Answer ans={} ) const noexcept
+    {
+        regex pattern( "^position=<\\s*(-?\\d+),\\s*(-?\\d+)\\s*>\\s*velocity=<\\s*(-?\\d+),\\s*(-?\\d+)\\s*>$" );
+        smatch group;
+        istringstream inputStream{ input };
+        for( string line; getline( inputStream, line ); )
+        {
+            if( regex_match( line, group, pattern ) && group.size() == 5 )
+            {
+                stringstream parser;
+                parser << group[ 1 ] << ' ' << group[ 2 ] << ' ' << group[ 3 ] << ' ' << group[ 4 ];
+                parser >> col >> row; Position pos{ row, col };
+                parser >> col >> row; Velocity speed{ row, col };
+                lights.emplace_back( Light{ std::move( pos ), std::move( speed ) } );
+            }
+        }
+
+        for( auto tick{ 0 }; tick < 5; ++tick )
+        {
+            auto uniqueTotal{ 0 }; Unique uniqueRow, uniqueCol;
+            for( const auto& light: lights )
+                uniqueRow.insert( light.pos.row ),
+                uniqueCol.insert( light.pos.col );
+            unaligned.push_back( uniqueRow.size() + uniqueCol.size() );
+            int unalignedCount = *min_element( unaligned.cbegin(), unaligned.cend() );
+            if( minUnaligned > unalignedCount )
+            {
+                minUnaligned = unalignedCount;
+                ans.lights = lights;
+                ans.tick = tick;
+                auto[ minRowIt, maxRowIt ] = minmax_element( uniqueRow.cbegin(), uniqueRow.cend() );
+                auto[ minColIt, maxColIt ] = minmax_element( uniqueCol.cbegin(), uniqueCol.cend() );
+                ans.minRow = *minRowIt, ans.maxRow = *maxRowIt;
+                ans.minCol = *minColIt, ans.maxCol = *maxColIt;
+            }
+
+            for( auto& light: lights )
+                light.pos.row += light.speed.row,
+                light.pos.col += light.speed.col;
+        }
+
+        return ans;
+    }
+};
+
+bool operator==( const Position& lhs, const Position& rhs ){ return lhs.row == rhs.row && lhs.col == rhs.col; }
+struct Cmp{ size_t operator()( const Position& pos ) const { return pos.row * 10000 + pos.col; } };
+int main()
+{
+
+    Solution s;
+    auto ans{ s.getMaxAligned( INPUT ) };
+    unordered_set< Position, Cmp > message;
+    for( const auto& light: ans.lights )
+        message.insert( light.pos );
+
+    for( auto i{ ans.minRow }; i <= ans.maxRow; ++i )
+    {
+        for( auto j{ ans.minCol }; j <= ans.maxCol; ++j )
+        {
+            if( message.find( { i, j } ) == message.end() )
+            {
+                cout << '.';
+            }
+            else
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
