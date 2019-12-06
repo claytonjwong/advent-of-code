ca96eefc0b25482187d7614a7f4cd230e44e48e2
Clayton Wong <clayton.wong@ookla.com>
2019-12-06 08:32:27 -0800

    2019  Day 6:Universal Orbit Map

---
+++ 2019/06_universal_orbit_map/main.js
@@ -0,0 +1,29 @@
+/*
+ * Day 6: Universal Orbit Map
+ *
+ * Q: https://adventofcode.com/2019/day/6
+ * A: https://claytonjwong.github.io/advent-of-code/2019/#day-6-universal-orbit-map
+ */ 
+let fs = require('fs');
+let input = fs.readFileSync('input.txt', 'utf-8').split("\n").map(edge => edge.split(')'));
+let edges = new Map(); // [child, parent] (ie. parent -> child)
+for (let [u, v] of input) edges.set(v, u); // u is the parent of v (ie. u -> v)
+let go = (edges, child) => !edges.get(child) ? 0 : 1 + go(edges, edges.get(child)); // parent = edges.get(child)
+let total = [...edges.keys()].map(child => go(edges, child)).reduce((a, b) => a + b);
+console.log(`Part 1: ${total}`);
+let [you, san] = [edges.get('YOU'), edges.get('SAN')];
+let path = { you: [], san: [] };
+while (you) path.you.unshift(you), you = edges.get(you); // push you's parents to front of path you
+while (san) path.san.unshift(san), san = edges.get(san); // push san's parents to front of path san
+let ancestor = null; // traverse paths from root till divergence to find the first common ancestor
+for (let i=0;; ++i) {
+    if (path.you[i] != path.san[i]) {
+        ancestor = path.you[i-1]; // same as path_san[i-1] (ie. you and san share this first common ancestor)
+        break;
+    }
+}
+let steps = { you: 0, san: 0 };
+[you, san] = [edges.get('YOU'), edges.get('SAN')];
+while (you != ancestor) ++steps.you, you = edges.get(you);
+while (san != ancestor) ++steps.san, san = edges.get(san);
+console.log(`Part 2: ${steps.you + steps.san}`);
