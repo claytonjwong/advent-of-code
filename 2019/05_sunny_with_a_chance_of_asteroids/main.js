b3c80149b5a6a79c67035ec3f7a4fbea064d46f7
Clayton Wong <clayton.wong@ookla.com>
2019-12-08 07:51:34 -0800

    minor edit, move exit condition (op == 99) into for-loop to remove one line

--- 2019/05_sunny_with_a_chance_of_asteroids/main.js
+++ 2019/05_sunny_with_a_chance_of_asteroids/main.js
@@ -13,7 +13,7 @@ let run = (id, ...A) => {
     return padded.substring(padded.length - 5);
   };
   let op = 0, instructions = [0, 4, 4, 2, 2, 0, 0, 4, 4];
-  for (let i = 0;; i += instructions[op]) {
+  for (let i = 0; op != 99; i += instructions[op]) {
     let cmd = pad(A[i]);
     let [u, v, w] = [A[i + 1], A[i + 2], A[i + 3]];
     u = cmd[2] == 0 ? A[u] : u;
@@ -27,7 +27,6 @@ let run = (id, ...A) => {
     if (op == 6) i = (u == 0) ? v : i + 3;
     if (op == 7) A[w] = u < v;
     if (op == 8) A[w] = u == v;
-    if (op == 99) break;
   }
   return ans;
 };
