37320c9eb6213e78b249d6eeaafd98d9c02acdd1
claytonjwong <claytonjwong@gmail.com>
2022-12-19 08:50:15 -0700

    Day 19: Not Enough Minerals

--- 2022/19_Not_Enough_Minerals/main.py
+++ 2022/19_Not_Enough_Minerals/main.py
@@ -2,116 +2,71 @@
 # https://adventofcode.com/2022/day/19
 #
 
-import os
-os.chdir('/Users/claytonjwong/sandbox/advent-of-code/2022/19_Not_Enough_Minerals')
-
 from collections import deque
 import re
 
-REGEX = '^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$'
-ORE, CLAY, OBS, GEO = 'ore', 'clay', 'obs', 'geo'
-ROCKS = [ORE, CLAY, OBS, GEO]
-
-key = lambda have: (have[ORE], have[CLAY], have[OBS], have[GEO])
-def obj(key):
-    ore, clay, obs, geo = key
-    return { ORE: ore, CLAY: clay, OBS: obs, GEO: geo, }
-
-def run(id, rocks, robot, needs, MINUTES = 24):
-    # print(f'run(id: {id}, rocks: {rocks}, robot: {robot}, needs: {needs})')
+def run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, T):
     best = 0
-    q, seen = deque([(0, key(rocks), key(robot))]), set()
+    # state is (ore, clay, obs, geo, r1, r2, r3, r4, time)
+    state = (0, 0, 0, 0, 1, 0, 0, 0, T)
+    q, seen = deque([state]), set()
     while q:
-        print(len(q))
-        minute, rocks, robot = q.popleft()
-        rocks, robot = [obj(have) for have in [rocks, robot]]
-        # print(rocks, robot)
-        if MINUTES < minute:
+        state = q.popleft()
+        ore, clay, obs, geo, r1, r2, r3, r4, t = state
+        best = max(best, geo)
+        if not t:
             continue
-        minute += 1
-        # make ore robot
-        if needs[ORE][ORE] <= rocks[ORE]:
-            next_rocks = rocks.copy(); next_rocks[ORE] -= needs[ORE][ORE]
-            next_robot = robot.copy(); next_robot[ORE] += 1
-            # collect rocks
-            for rock in robot.keys():
-                next_rocks[rock] += robot[rock]
-            best = max(best, next_rocks[GEO])
-            next_state = (minute, key(next_rocks), key(next_robot))
-            next_key = (key(next_rocks), key(next_robot))
-            if next_key not in seen:
-                q.append(next_state); seen.add(next_key)
-        # make clay robot
-        if needs[CLAY][ORE] <= rocks[ORE]:
-            next_rocks = rocks.copy(); next_rocks[ORE] -= needs[CLAY][ORE]
-            next_robot = robot.copy(); next_robot[CLAY] += 1
-            for rock in robot.keys():
-                next_rocks[rock] += robot[rock]
-            best = max(best, next_rocks[GEO])
-            next_state = (minute, key(next_rocks), key(next_robot))
-            next_key = (key(next_rocks), key(next_robot))
-            if next_key not in seen:
-                q.append(next_state); seen.add(next_key)
-        # make obs robot
-        if needs[OBS][ORE] <= rocks[ORE] and needs[OBS][CLAY] <= rocks[CLAY]:
-            next_rocks = rocks.copy(); next_rocks[ORE] -= needs[OBS][ORE]; next_rocks[CLAY] -= needs[OBS][CLAY]
-            next_robot = robot.copy(); next_robot[OBS] += 1
-            for rock in robot.keys():
-                next_rocks[rock] += robot[rock]
-            best = max(best, next_rocks[GEO])
-            next_state = (minute, key(next_rocks), key(next_robot))
-            next_key = (key(next_rocks), key(next_robot))
-            if next_key not in seen:
-                q.append(next_state); seen.add(next_key)
-        # make geo robot
-        if needs[GEO][ORE] <= rocks[ORE] and needs[GEO][OBS] <= rocks[OBS]:
-            next_rocks = rocks.copy(); next_rocks[ORE] -= needs[GEO][ORE]; next_rocks[OBS] -= needs[GEO][OBS]
-            next_robot = robot.copy(); next_robot[GEO] += 1
-            for rock in robot.keys():
-                next_rocks[rock] += robot[rock]
-            best = max(best, next_rocks[GEO])
-            next_state = (minute, key(next_rocks), key(next_robot))
-            next_key = (key(next_rocks), key(next_robot))
-            if next_key not in seen:
-                q.append(next_state); seen.add(next_key)
-        # save rocks without buying a robot
-        for rock in robot.keys():
-            rocks[rock] += robot[rock]
-        best = max(best, rocks[GEO])
-        next_state = (minute, key(rocks), key(robot))
-        next_key = (key(rocks), key(robot))
-        if next_key not in seen:
-            q.append(next_state); seen.add(next_key)
-    # for minute, rocks, robot in seen:
-    #     if minute == 11:
-    #         print(rocks, robot)
+
+        # BFS pruning
+        hi = max([ore_cost_ore, clay_cost_ore, obs_cost_ore, geo_cost_ore])
+        if r1 >= hi:
+            r1 = hi
+        if r2 >= obs_cost_clay:
+            r2 = obs_cost_clay
+        if r3 >= geo_cost_obs:
+            r3 = geo_cost_obs
+        if ore >= t * hi - r1 * (t - 1):
+            ore = t * hi - r1 * (t - 1)
+        if clay >= t * obs_cost_clay - r2 * (t - 1):
+            clay = t * obs_cost_clay - r2 * (t - 1)
+        if obs >= t * geo_cost_obs - r3 * (t - 1):
+            obs = t * geo_cost_obs - r3 * (t - 1)
+
+        state = (ore, clay, obs, geo, r1, r2, r3, r4, t)
+        if state in seen:
+            continue
+        seen.add(state)
+
+        # buy nothing
+        q.append((ore + r1, clay + r2, obs + r3, geo + r4, r1, r2, r3, r4, t - 1))
+        # buy ore robot
+        if ore_cost_ore <= ore: q.append((ore - ore_cost_ore + r1, clay + r2, obs + r3, geo + r4, r1 + 1, r2, r3, r4, t - 1))
+        # buy clay robot
+        if clay_cost_ore <= ore: q.append((ore - clay_cost_ore + r1, clay + r2, obs + r3, geo + r4, r1, r2 + 1, r3, r4, t - 1))
+        # buy obs robot
+        if obs_cost_ore <= ore and obs_cost_clay <= clay: q.append((ore - obs_cost_ore + r1, clay - obs_cost_clay + r2, obs + r3, geo + r4, r1, r2, r3 + 1, r4, t - 1))
+        # buy geo robot
+        if geo_cost_ore <= ore and geo_cost_obs <= obs: q.append((ore - geo_cost_ore + r1, clay + r2, obs - geo_cost_obs + r3, geo + r4, r1, r2, r3, r4 + 1, t - 1))
     return best
 
-A = []
+part1, part2 = 0, 1
 with open('input.txt') as input:
-    for line in input:
-        match = re.search(REGEX, line.strip())
-        id = int(match.group(1))
-        rocks = {rock: 0 for rock in ROCKS}
-        robot = {rock: 0 for rock in ROCKS}; robot[ORE] = 1
-        needs = {
-            ORE: {
-                ORE: int(match.group(2))
-            },
-            CLAY: {
-                ORE: int(match.group(3)),
-            },
-            OBS: {
-                ORE: int(match.group(4)),
-                CLAY: int(match.group(5)),
-            },
-            GEO: {
-                ORE: int(match.group(6)),
-                OBS: int(match.group(7)),
-            },
-        }
-        geos = run(id, rocks, robot, needs)
-        print(f'geos: {geos}')
-        A.append(geos)
-
-
+    for i, line in enumerate(input):
+        REGEX = '^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$'
+        m = re.search(REGEX, line.strip())
+        id = int(m.group(1))
+        ore_cost_ore = int(m.group(2))
+        clay_cost_ore = int(m.group(3))
+        obs_cost_ore = int(m.group(4))
+        obs_cost_clay = int(m.group(5))
+        geo_cost_ore = int(m.group(6))
+        geo_cost_obs = int(m.group(7))
+        best = run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, 24)
+        part1 += id * best
+        if i < 3:
+            best = run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, 32)
+            part2 *= best
+print(f'part 1: {part1}')
+print(f'part 2: {part2}')
+# part 1: 1264
+# part 2: 13475
--- 2022/README.md
+++ 2022/README.md
@@ -766,3 +766,75 @@ for k in m.keys():
 print(f'part 1: {t}')
 # part 1: 3500
 ```
+
+## [Day 19: Not Enough Minerals](https://adventofcode.com/2022/day/19)
+```python
+from collections import deque
+import re
+
+def run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, T):
+    best = 0
+    # state is (ore, clay, obs, geo, r1, r2, r3, r4, time)
+    state = (0, 0, 0, 0, 1, 0, 0, 0, T)
+    q, seen = deque([state]), set()
+    while q:
+        state = q.popleft()
+        ore, clay, obs, geo, r1, r2, r3, r4, t = state
+        best = max(best, geo)
+        if not t:
+            continue
+
+        # BFS pruning
+        hi = max([ore_cost_ore, clay_cost_ore, obs_cost_ore, geo_cost_ore])
+        if r1 >= hi:
+            r1 = hi
+        if r2 >= obs_cost_clay:
+            r2 = obs_cost_clay
+        if r3 >= geo_cost_obs:
+            r3 = geo_cost_obs
+        if ore >= t * hi - r1 * (t - 1):
+            ore = t * hi - r1 * (t - 1)
+        if clay >= t * obs_cost_clay - r2 * (t - 1):
+            clay = t * obs_cost_clay - r2 * (t - 1)
+        if obs >= t * geo_cost_obs - r3 * (t - 1):
+            obs = t * geo_cost_obs - r3 * (t - 1)
+
+        state = (ore, clay, obs, geo, r1, r2, r3, r4, t)
+        if state in seen:
+            continue
+        seen.add(state)
+
+        # buy nothing
+        q.append((ore + r1, clay + r2, obs + r3, geo + r4, r1, r2, r3, r4, t - 1))
+        # buy ore robot
+        if ore_cost_ore <= ore: q.append((ore - ore_cost_ore + r1, clay + r2, obs + r3, geo + r4, r1 + 1, r2, r3, r4, t - 1))
+        # buy clay robot
+        if clay_cost_ore <= ore: q.append((ore - clay_cost_ore + r1, clay + r2, obs + r3, geo + r4, r1, r2 + 1, r3, r4, t - 1))
+        # buy obs robot
+        if obs_cost_ore <= ore and obs_cost_clay <= clay: q.append((ore - obs_cost_ore + r1, clay - obs_cost_clay + r2, obs + r3, geo + r4, r1, r2, r3 + 1, r4, t - 1))
+        # buy geo robot
+        if geo_cost_ore <= ore and geo_cost_obs <= obs: q.append((ore - geo_cost_ore + r1, clay + r2, obs - geo_cost_obs + r3, geo + r4, r1, r2, r3, r4 + 1, t - 1))
+    return best
+
+part1, part2 = 0, 1
+with open('input.txt') as input:
+    for i, line in enumerate(input):
+        REGEX = '^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$'
+        m = re.search(REGEX, line.strip())
+        id = int(m.group(1))
+        ore_cost_ore = int(m.group(2))
+        clay_cost_ore = int(m.group(3))
+        obs_cost_ore = int(m.group(4))
+        obs_cost_clay = int(m.group(5))
+        geo_cost_ore = int(m.group(6))
+        geo_cost_obs = int(m.group(7))
+        best = run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, 24)
+        part1 += id * best
+        if i < 3:
+            best = run(ore_cost_ore, clay_cost_ore, obs_cost_ore, obs_cost_clay, geo_cost_ore, geo_cost_obs, 32)
+            part2 *= best
+print(f'part 1: {part1}')
+print(f'part 2: {part2}')
+# part 1: 1264
+# part 2: 13475
+```
