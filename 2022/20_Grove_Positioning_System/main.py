4a05b7c5cd187f39097197cd9d9dde9d1ba72986
claytonjwong <claytonjwong@gmail.com>
2022-12-20 09:22:25 -0700

    Day 20: Grove Positioning System

--- 2022/20_Grove_Positioning_System/main.py
+++ 2022/20_Grove_Positioning_System/main.py
@@ -2,52 +2,31 @@
 # https://adventofcode.com/2022/day/20
 #
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.prev = None
-        self.next = None
-
 A = []
 with open('input.txt') as input:
     for line in input:
-        A.append(Node(int(line)))
-head = A[0]
-node = head
+        A.append(int(line))
 N = len(A)
-for i in range(N):
-    A[i].prev = A[i - 1]
-    A[i].next = A[(i + 1) % N]
 
-for i in range(N):
-    node = A[i]
-    if not A[i].val:
-        continue
-    for _ in range(abs(A[i].val)):
-        if 0 < A[i].val: node = node.next
-        if A[i].val < 0: node = node.prev
-    remove = A[i]
-    remove.prev.next = remove.next
-    remove.next.prev = remove.prev
-    insert = remove
-    if 0 < A[i].val:
-        temp = node.next
-        node.next = insert
-        insert.next = temp; temp.prev = insert
-        insert.prev = node
-    if A[i].val < 0:
-        temp = node.prev
-        node.prev = insert
-        insert.prev = temp; temp.next = insert
-        insert.next = node
+def mix(A, cnt = 1, key = 1):
+    A = [x * key for x in A]
+    I = [i for i in range(N)]
+    for _ in range(cnt):
+        for i, steps in enumerate(A):
+            if not steps:
+                continue
+            j = I.index(i)
+            k = (j + steps) % (N - 1)
+            l = I.pop(j)
+            I.insert(k, l)
+    return [A[i] for i in I]
 
-node = A[0]
-while node.val:
-    node = node.next
+def run(A, cnt = 1, key = 1):
+    A = mix(A, cnt, key)
+    i = A.index(0)
+    return sum(A[(i + k) % N] for k in range(3000 + 1) if k in [1000, 2000, 3000])
 
-t = 0
-for k in range(3000 + 1):
-    if k == 1000 or k == 2000 or k == 3000:
-        t += node.val
-    node = node.next
-print(f'part 1: {t}')
+print(f'part 1: {run(A[:])}')
+print(f'part 2: {run(A[:], 10, 811589153)}')
+# part 1: 3346
+# part 2: 4265712588168
--- 2022/README.md
+++ 2022/README.md
@@ -838,3 +838,35 @@ print(f'part 2: {part2}')
 # part 1: 1264
 # part 2: 13475
 ```
+
+## [Day 20: Grove Positioning System](https://adventofcode.com/2022/day/20)
+```python
+A = []
+with open('input.txt') as input:
+    for line in input:
+        A.append(int(line))
+N = len(A)
+
+def mix(A, cnt = 1, key = 1):
+    A = [x * key for x in A]
+    I = [i for i in range(N)]
+    for _ in range(cnt):
+        for i, steps in enumerate(A):
+            if not steps:
+                continue
+            j = I.index(i)
+            k = (j + steps) % (N - 1)
+            l = I.pop(j)
+            I.insert(k, l)
+    return [A[i] for i in I]
+
+def run(A, cnt = 1, key = 1):
+    A = mix(A, cnt, key)
+    i = A.index(0)
+    return sum(A[(i + k) % N] for k in range(3000 + 1) if k in [1000, 2000, 3000])
+
+print(f'part 1: {run(A[:])}')
+print(f'part 2: {run(A[:], 10, 811589153)}')
+# part 1: 3346
+# part 2: 4265712588168
+```
