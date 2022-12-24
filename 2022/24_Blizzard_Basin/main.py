90c403115173a6d7443585ec0d571af965c45752
claytonjwong <claytonjwong@gmail.com>
2022-12-24 08:49:55 -0700

    Day 24: Blizzard Basin (part 2 + cleanup)

--- 2022/24_Blizzard_Basin/main.py
+++ 2022/24_Blizzard_Basin/main.py
@@ -2,16 +2,11 @@
 # https://adventofcode.com/2022/day/24
 #
 
-import os
-os.chdir('/Users/claytonjwong/sandbox/advent-of-code/2022/24_Blizzard_Basin')
-
-from collections import deque
-
 A = []
 with open('input.txt') as input:
-    for line in input:
-        A.append(list(line.strip()))
+    A = [list(line.strip()) for line in input]
 M, N = len(A), len(A[0])
+S, T = (0, A[0].index('.')), (M - 1, A[M - 1].index('.')) # source, target
 
 class Blizzard:
     def __init__(self, i, j, d):
@@ -23,31 +18,29 @@ class Blizzard:
         self.i += self.di
         self.j += self.dj
         if A[self.i][self.j] == '#': # wrap-around assuming no up/down in source, target columns
-            self.i += self.di
-            self.j += self.dj
-            if self.i < 0: self.i = M - 2
-            if self.j < 0: self.j = N - 2
-            if self.i == M: self.i = 1
-            if self.j == N: self.j = 1
+            if self.i == 0: self.i = M - 2
+            if self.j == 0: self.j = N - 2
+            if self.i == M - 1: self.i = 1
+            if self.j == N - 1: self.j = 1
         return (self.i, self.j)
+B = [Blizzard(i, j, A[i][j]) for j in range(1, N - 1) for i in range(1, M - 1) if A[i][j] != '.']
 
-B = []
-for i in range(1, M - 1):
-    for j in range(1, N - 1):
-        if A[i][j] != '.':
-            B.append(Blizzard(i, j, A[i][j]))
+def run(S, T):
+    cur, step = set([S]), 0
+    while len(cur):
+        next, bliz = set(), set([b.step() for b in B])
+        for i, j in cur:
+            if (i, j) == T:
+                return step
+            for u, v in [(i, j), (i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
+                if 0 <= u < M and 0 <= v < N and A[u][v] != '#' and (u, v) not in bliz:
+                    next.add((u, v))
+        cur = next; step += 1
 
-S, T = (0, A[0].index('.')), (M - 1, A[M - 1].index('.')) # source, target
-cur, step = set([S]), -1
-while len(cur):
-    step += 1
-    bliz, next = set([b.step() for b in B]), set()
-    for i, j in cur:
-        if (i, j) == T:
-            next = set()
-            break
-        for u, v in [(i, j), (i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
-            if 0 <= u < M and 0 <= v < N and A[u][v] != '#' and (u, v) not in bliz:
-                next.add((u, v))
-    cur = next
-print(f'part 1: {step}')
+a = run(S, T)
+b = run(T, S) + 1
+c = run(S, T) + 1
+print(f'part 1: {a}')
+print(f'part 2: {a + b + c}')
+# part 1: 332
+# part 2: 942
--- 2022/README.md
+++ 2022/README.md
@@ -1080,3 +1080,49 @@ print(f'part 2: {part2}')
 # part 1: 3788
 # part 2: 921
 ```
+
+## [Day 24: Blizzard Basin](https://adventofcode.com/2022/day/24)
+```python
+A = []
+with open('input.txt') as input:
+    A = [list(line.strip()) for line in input]
+M, N = len(A), len(A[0])
+S, T = (0, A[0].index('.')), (M - 1, A[M - 1].index('.')) # source, target
+
+class Blizzard:
+    def __init__(self, i, j, d):
+        self.i = i
+        self.j = j
+        self.di = -1 if d == '^' else 1 if d == 'v' else 0
+        self.dj = -1 if d == '<' else 1 if d == '>' else 0
+    def step(self):
+        self.i += self.di
+        self.j += self.dj
+        if A[self.i][self.j] == '#': # wrap-around assuming no up/down in source, target columns
+            if self.i == 0: self.i = M - 2
+            if self.j == 0: self.j = N - 2
+            if self.i == M - 1: self.i = 1
+            if self.j == N - 1: self.j = 1
+        return (self.i, self.j)
+B = [Blizzard(i, j, A[i][j]) for j in range(1, N - 1) for i in range(1, M - 1) if A[i][j] != '.']
+
+def run(S, T):
+    cur, step = set([S]), 0
+    while len(cur):
+        next, bliz = set(), set([b.step() for b in B])
+        for i, j in cur:
+            if (i, j) == T:
+                return step
+            for u, v in [(i, j), (i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
+                if 0 <= u < M and 0 <= v < N and A[u][v] != '#' and (u, v) not in bliz:
+                    next.add((u, v))
+        cur = next; step += 1
+
+a = run(S, T)
+b = run(T, S) + 1
+c = run(S, T) + 1
+print(f'part 1: {a}')
+print(f'part 2: {a + b + c}')
+# part 1: 332
+# part 2: 942
+```
