c657bfec5333593d362d0d7a5d773d8c5c61860f
claytonjwong <claytonjwong@gmail.com>
2023-12-04 07:52:12 -0700

    minor edit: rename 'num' to 'card' for readability

--- 2023/04_Scratchcards/main.py
+++ 2023/04_Scratchcards/main.py
@@ -5,15 +5,15 @@ cnt, hi = Counter(), 0
 with open('input.txt') as input:
     for line in input:
         L, R = line.strip().split('|')
-        num = int([s for s in L.split(':')[0].split()][1]); cnt[num] += 1; hi = max(hi, num)
+        card = int([s for s in L.split(':')[0].split()][1]); cnt[card] += 1; hi = max(hi, card)
         need = set(int(s) for s in L.split(':')[1].split())
         have = set(int(s) for s in R.split())
         same = need & have
         t1 += 1 << (len(same) - 1) if len(same) else 0
         for i in range(len(same)):
-            take = num + i + 1
-            cnt[take] += cnt[num]
-t2 = sum(freq for num, freq in cnt.items() if num <= hi)
+            take = card + i + 1
+            cnt[take] += cnt[card]
+t2 = sum(freq for card, freq in cnt.items() if card <= hi)
 
 print(f'part 1: {t1}')
 print(f'part 2: {t2}')
