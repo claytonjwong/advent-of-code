5c18b73295cb72e88f483c15e4e038f3c642a4ba
claytonjwong <claytonjwong@gmail.com>
2023-12-05 10:21:57 -0700

    make code more consise -- WTF dude? (again for part 2 👻)

--- 2023/01_Trebuchet/main.py
+++ 2023/01_Trebuchet/main.py
@@ -2,39 +2,21 @@
 # https://adventofcode.com/2023/day/1
 #
 
-
-#
-# part 1
-#
-
-t = 0
+t1 = 0
+t2 = 0
 with open('input.txt') as input:
+    m = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
     for s in input:
         digits = [c for c in s if c.isdigit()]
-        t += int(f'{digits[0]}{digits[-1]}')
-print(f'part 1: {t}')
-
-
-#
-# part 2
-#
-
-def f(s, reverse = False):
-    m = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
-    n = len(s)
-    for i in reversed(range(n)) if reverse else range(n):
-        if s[i].isdigit():
-            return int(s[i])
-        for k in m.keys():
-            if i + len(k) <= n and s[i:i+len(k)] == k:
-                return m[k]
-    return -1
-
-t = 0
-with open('input.txt') as input:
-    for s in input:
-        beg, end = f(s), f(s, True)
-        t += int(f'{beg}{end}')
-print(f'part 2: {t}')
+        t1 += int(f'{digits[0]}{digits[-1]}')
+        digits = []
+        for i in range(len(s)):
+            for k in range(1, len(s) + 1 - i):
+                it = s[i:i + k]
+                if it in m:
+                    digits.append(m[it])
+        t2 += int(f'{digits[0]}{digits[-1]}')
+print(f'part 1: {t1}')
+print(f'part 2: {t2}')
 # part 1: 55017
 # part 2: 53539
