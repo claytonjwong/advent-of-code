c4064dd17e2ad4a5704251c5bb8913785bb41367
claytonjwong <claytonjwong@gmail.com>
2023-12-08 09:40:26 -0700

    Day 8: Haunted Wasteland (part 2: brute-force TLE)

--- 2023/08_Haunted_Wasteland/main.py
+++ 2023/08_Haunted_Wasteland/main.py
@@ -27,3 +27,15 @@ while node.name != 'ZZZ':
     if dirs[i] == 'R': node = node.R
 print(f'part 1: {step}')
 # part 1: 13301
+
+step, pre = 0, [node for name, node in nodes.items() if name[-1] == 'A']
+while not all(node.name[-1] == 'Z' for node in pre):
+    i = step % len(dirs); step += 1; cur = []
+    if not (step % len(dirs)):
+        print(f'step {step}: A: {[node.name for node in pre]}')
+    for node in pre:
+        if dirs[i] == 'L': cur.append(node.L)
+        if dirs[i] == 'R': cur.append(node.R)
+    pre = cur
+print(f'part 2: {step}')
+# TODO: this step is taking forever to run, maybe look for alternative patterns such as pisano period to derive a solution?
