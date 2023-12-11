9fd867899869c11fab579c00414f5595bb8f0c8b
claytonjwong <claytonjwong@gmail.com>
2023-12-11 15:25:18 -0700

    Day 11: Cosmic Expansion (part 2)

--- 2023/11_Cosmic_Expansion/main.py
+++ 2023/11_Cosmic_Expansion/main.py
@@ -1,21 +1,12 @@
-text = []
-with open('input.txt') as input:
+A = []
+with open('/Users/claytonjwong/sandbox/advent-of-code/2023/11_Cosmic_Expansion/input.txt') as input:
     for line in input:
-        text.append(line.strip())
-m, n = len(text), len(text[0])
-row = set(i for i in range(m) if all(c == '.' for j in range(n) for c in text[i][j]))
-col = set(j for j in range(n) if all(c == '.' for i in range(m) for c in text[i][j]))
-M, N = m + len(row), n + len(col)
-A = [['.'] * N for _ in range(M)]
-
-u = 0
-for i in range(m):
-    v = 0
-    for j in range(n):
-        if text[i][j] == '#':
-            A[u][v] = '#'
-        v += 1 + int(j in col)
-    u += 1 + int(i in row)
+        A.append(line.strip())
+M, N = len(A), len(A[0])
+row = set(i for i in range(M) if all(c == '.' for j in range(N) for c in A[i][j]))
+col = set(j for j in range(N) if all(c == '.' for i in range(M) for c in A[i][j]))
+cost = int(1e6)
+# cost = 2
 
 t = 0
 V = set((i, j) for i in range(M) for j in range(N) if A[i][j] == '#')
@@ -23,6 +14,26 @@ for u in V:
     for v in V:
         if u == v:
             continue
-        t += abs(u[0] - v[0]) + abs(u[1] - v[1])  # manhattan distance from u -> v
+        di = 1 if u[0] < v[0] else -1 if v[0] < u[0] else 0
+        dj = 1 if u[1] < v[1] else -1 if v[1] < u[1] else 0
+        i, j = u
+        while i != v[0] or j != v[1]:
+            if i != v[0]:
+                i += di
+                # print(f'i: {i}')
+                # if i in row:
+                #     print(f'i: {i} in row')
+                t += cost if i in row else 1
+            if j != v[1]:
+                j += dj
+                # print(f'j: {j}')
+                # if j in col:
+                #     print(f'j: {j} in col')
+                t += cost if j in col else 1
+        # t += sum(cost if i in row else 1 for i in range(min(u[0], v[0]), max(u[0], v[0]) + 1))
+        # t += sum(cost if j in col else 1 for j in range(min(u[1], v[1]), max(u[1], v[1]) + 1))
+print(t)
+
 print(f'part 1: {t // 2}')  # divide by 2 since edges are bidirectional
 # part 1: 9608724
+# part 2: 904633799472
