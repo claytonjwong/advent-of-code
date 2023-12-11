2f16c0ca59ff0a0bf2fdcdf76ec06512f0a58dff
claytonjwong <claytonjwong@gmail.com>
2023-12-11 15:40:22 -0700

    Day 11: Cosmic Expansion (code cleanup)

--- 2023/11_Cosmic_Expansion/main.py
+++ 2023/11_Cosmic_Expansion/main.py
@@ -3,37 +3,25 @@ with open('/Users/claytonjwong/sandbox/advent-of-code/2023/11_Cosmic_Expansion/i
     for line in input:
         A.append(line.strip())
 M, N = len(A), len(A[0])
-row = set(i for i in range(M) if all(c == '.' for j in range(N) for c in A[i][j]))
-col = set(j for j in range(N) if all(c == '.' for i in range(M) for c in A[i][j]))
-cost = int(1e6)
-# cost = 2
 
-t = 0
+row, col = [], []
 V = set((i, j) for i in range(M) for j in range(N) if A[i][j] == '#')
 for u in V:
     for v in V:
-        if u == v:
-            continue
+        i, j = u
         di = 1 if u[0] < v[0] else -1 if v[0] < u[0] else 0
         dj = 1 if u[1] < v[1] else -1 if v[1] < u[1] else 0
-        i, j = u
         while i != v[0] or j != v[1]:
-            if i != v[0]:
-                i += di
-                # print(f'i: {i}')
-                # if i in row:
-                #     print(f'i: {i} in row')
-                t += cost if i in row else 1
-            if j != v[1]:
-                j += dj
-                # print(f'j: {j}')
-                # if j in col:
-                #     print(f'j: {j} in col')
-                t += cost if j in col else 1
-        # t += sum(cost if i in row else 1 for i in range(min(u[0], v[0]), max(u[0], v[0]) + 1))
-        # t += sum(cost if j in col else 1 for j in range(min(u[1], v[1]), max(u[1], v[1]) + 1))
-print(t)
+            if i != v[0]: i += di; row.append(i)
+            if j != v[1]: j += dj; col.append(j)
 
-print(f'part 1: {t // 2}')  # divide by 2 since edges are bidirectional
+cost1 = int(2e0)
+cost2 = int(1e6)
+SPACE_ROW = set(i for i in range(M) if all(c == '.' for j in range(N) for c in A[i][j]))
+SPACE_COL = set(j for j in range(N) if all(c == '.' for i in range(M) for c in A[i][j]))
+t1 = sum(cost1 if i in SPACE_ROW else 1 for i in row) + sum(cost1 if j in SPACE_COL else 1 for j in col)
+t2 = sum(cost2 if i in SPACE_ROW else 1 for i in row) + sum(cost2 if j in SPACE_COL else 1 for j in col)
+print(f'part 1: {t1 // 2}')
+print(f'part 2: {t2 // 2}')
 # part 1: 9608724
 # part 2: 904633799472
--- 2023/README.md
+++ 2023/README.md
@@ -414,32 +414,31 @@ print(f'part 1: {depth}')
 ## [Day 11: Cosmic Expansion](https://adventofcode.com/2023/day/11)
 
 ```python
-text = []
-with open('input.txt') as input:
+A = []
+with open('/Users/claytonjwong/sandbox/advent-of-code/2023/11_Cosmic_Expansion/input.txt') as input:
     for line in input:
-        text.append(line.strip())
-m, n = len(text), len(text[0])
-row = set(i for i in range(m) if all(c == '.' for j in range(n) for c in text[i][j]))
-col = set(j for j in range(n) if all(c == '.' for i in range(m) for c in text[i][j]))
-M, N = m + len(row), n + len(col)
-A = [['.'] * N for _ in range(M)]
+        A.append(line.strip())
+M, N = len(A), len(A[0])
 
-u = 0
-for i in range(m):
-    v = 0
-    for j in range(n):
-        if text[i][j] == '#':
-            A[u][v] = '#'
-        v += 1 + int(j in col)
-    u += 1 + int(i in row)
-
-t = 0
+row, col = [], []
 V = set((i, j) for i in range(M) for j in range(N) if A[i][j] == '#')
 for u in V:
     for v in V:
-        if u == v:
-            continue
-        t += abs(u[0] - v[0]) + abs(u[1] - v[1])  # manhattan distance from u -> v
-print(f'part 1: {t // 2}')  # divide by 2 since edges are bidirectional
+        di = 1 if u[0] < v[0] else -1 if v[0] < u[0] else 0
+        dj = 1 if u[1] < v[1] else -1 if v[1] < u[1] else 0
+        i, j = u
+        while i != v[0] or j != v[1]:
+            if i != v[0]: i += di; row.append(i)
+            if j != v[1]: j += dj; col.append(j)
+
+cost1 = int(2e0)
+cost2 = int(1e6)
+SPACE_ROW = set(i for i in range(M) if all(c == '.' for j in range(N) for c in A[i][j]))
+SPACE_COL = set(j for j in range(N) if all(c == '.' for i in range(M) for c in A[i][j]))
+t1 = sum(cost1 if i in SPACE_ROW else 1 for i in row) + sum(cost1 if j in SPACE_COL else 1 for j in col)
+t2 = sum(cost2 if i in SPACE_ROW else 1 for i in row) + sum(cost2 if j in SPACE_COL else 1 for j in col)
+print(f'part 1: {t1 // 2}')
+print(f'part 2: {t2 // 2}')
 # part 1: 9608724
+# part 2: 904633799472
 ```
