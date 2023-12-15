96f171f52aab8c61ccc2b35b988018e0609b4820
claytonjwong <claytonjwong@gmail.com>
2023-12-15 16:30:47 -0700

    Day 14: Parabolic Reflector Dish (part 2)

--- 2023/14_Parabolic_Reflector_Dish/main.py
+++ 2023/14_Parabolic_Reflector_Dish/main.py
@@ -1,29 +1,34 @@
-# 3:32pm - start reading the problem statement
-# 3:41pm - ~9 minutes to read + game plan
-
-# move each O "up" <-- keep it super simple
-
-# 3:42pm -
-# 3:49pm - ~7 minutes for the implementation in Python3 which is AC :)
-
-# 3:50pm - part 2
-
 A = []
 with open('input.txt') as input:
     for line in input:
         A.append([c for c in line.strip()])
 M, N = len(A), len(A[0])
 
+total = lambda A: sum(M - i for i in range(M) for j in range(N) if A[i][j] == 'O')
+
 def step(i, j, di, dj):
+    if A[i][j] != 'O':
+        return
     u, v = i + di, j + dj
     while 0 <= u < M and 0 <= v < N and A[u][v] == '.':
         A[i][j] = '.'; A[u][v] = 'O'
         i, j = u, v; u, v = i + di, j + dj
-for i in range(M):
-    for j in range(N):
-        if A[i][j] == 'O':
-            step(i, j, di=-1, dj=0)
 
-t1 = sum(M - i for i in range(M) for j in range(N) if A[i][j] == 'O')
-print(f'part 1: {t1}')
+def north(A):
+    for i in range(M):
+        for j in range(N):
+            step(i, j, di=-1, dj=0)
+    return total(A)
+
+def cycle(A):
+    for _ in range(1000):
+        for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            for i in range(M) if di == -1 else reversed(range(M)):
+                for j in range(N) if dj == -1 else reversed(range(N)):
+                    step(i, j, di, dj)
+    return total(A)
+
+print(f'part 1: {north(A[:])}')
+print(f'part 2: {cycle(A[:])}')
 # part 1: 109424
+# part 2: 102509
--- 2023/README.md
+++ 2023/README.md
@@ -515,3 +515,44 @@ cols1 = sum(col(A) for A in matrix)
 print(f'part 1: {100 * rows1 + cols1}')
 # part 1: 32371
 ```
+
+---
+
+[Day 14: Parabolic Reflector Dish](https://adventofcode.com/2023/day/14)
+
+```python
+A = []
+with open('input.txt') as input:
+    for line in input:
+        A.append([c for c in line.strip()])
+M, N = len(A), len(A[0])
+
+total = lambda A: sum(M - i for i in range(M) for j in range(N) if A[i][j] == 'O')
+
+def step(i, j, di, dj):
+    if A[i][j] != 'O':
+        return
+    u, v = i + di, j + dj
+    while 0 <= u < M and 0 <= v < N and A[u][v] == '.':
+        A[i][j] = '.'; A[u][v] = 'O'
+        i, j = u, v; u, v = i + di, j + dj
+
+def north(A):
+    for i in range(M):
+        for j in range(N):
+            step(i, j, di=-1, dj=0)
+    return total(A)
+
+def cycle(A):
+    for _ in range(1000):
+        for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            for i in range(M) if di == -1 else reversed(range(M)):
+                for j in range(N) if dj == -1 else reversed(range(N)):
+                    step(i, j, di, dj)
+    return total(A)
+
+print(f'part 1: {north(A[:])}')
+print(f'part 2: {cycle(A[:])}')
+# part 1: 109424
+# part 2: 102509
+```
