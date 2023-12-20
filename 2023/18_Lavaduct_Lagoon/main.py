b70ad9485ae16b736bdbd6e9cc4d8838ce783cb2
claytonjwong <claytonjwong@gmail.com>
2023-12-20 13:13:24 -0700

    Day 18: Lavaduct Lagoon (part 1)

--- 2023/18_Lavaduct_Lagoon/main.py
+++ 2023/18_Lavaduct_Lagoon/main.py
@@ -11,6 +11,12 @@
 
 # 12:51pm => oops, I need to remove outside from above and below as well...
 
+# no that was an incorrect algorithm, so instead just flood fill with DFS
+
+# .................................................................#.
+
+# [1, 68]
+
 i, j = 0, 0
 have, first = set([(0, 0)]), True
 with open('input.txt') as input:
@@ -23,33 +29,26 @@ with open('input.txt') as input:
             i += di
             j += dj
             have.add((i, j))
-
 lo_i, hi_i = min(i for i, _ in have), max(i for i, _ in have)
 lo_j, hi_j = min(j for _, j in have), max(j for _, j in have)
 
-for i in range(lo_i, hi_i + 1):
-    print(''.join(['#' if (i, j) in have else '.' for j in range(lo_j, hi_j + 1)]))
+print(f'have: {len(have)}')
+print(f'i from {lo_i}..{hi_i}')
+print(f'j from {lo_j}..{hi_j}')
 
-# t, outside = (hi_i - lo_i + 1) * (hi_j - lo_j + 1), 0
-# for i in range(lo_i, hi_i + 1):
-#     for j in range(lo_j, hi_j + 1):
-#         if (i, j) in have:
-#             break
-#         outside += 1
-#     for j in reversed(range(lo_j, hi_j + 1)):
-#         if (i, j) in have:
-#             break
-#         outside += 1
-# for j in range(lo_j, hi_j + 1):
-#     for i in range(lo_i, hi_i + 1):
-#         if (i, j) in have:
-#             break
-#         outside += 1
-#     for i in reversed(range(lo_i, hi_i + 1)):
-#         if (i, j) in have:
-#             break
-#         outside += 1
-# inside = t - outside
+# .................................#
+
+import sys
+sys.setrecursionlimit(int(1e6))
+def go(i = lo_i + 69, j = lo_j + 69):
+    have.add((i, j))
+    print(f'go({i},{j}) have: {len(have)}')
+    for u, v in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:
+        if (u, v) not in have:
+            go(u, v)
+go()
 # print(f'part 1: {inside}')
 # part 1: 68624 is too high
 # part 1: 24445 is too low
+
+print(f'part 1: {len(have)}')
