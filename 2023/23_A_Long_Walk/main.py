2f2948128079bd45f5a266c4f7cbe97f2a076119
claytonjwong <claytonjwong@gmail.com>
2023-12-23 15:18:40 -0700

    Day 23: A Long Walk (part 1 -- cleanup + add to README)

--- 2023/23_A_Long_Walk/main.py
+++ 2023/23_A_Long_Walk/main.py
@@ -11,19 +11,17 @@ S = (0, [j for j in range(N) if A[0][j] == '.'][0])
 T = (M - 1, [j for j in range(N) if A[M - 1][j] == '.'][0])
 
 seen = set()
-def go(i = S[0], j = S[1], k = 0):
-    if (i, j) == T:
-        return k
-    if i < 0 or j < 0 or i == M or j == N or A[i][j] == '#' or (i, j) in seen:
+def go(i = S[0], j = S[1]):
+    if i < 0 or j < 0 or i == M or j == N or A[i][j] == '#' or (i, j) in seen or (i, j) == T:
         return 0
     best = 0
     seen.add((i, j))
-    if   A[i][j] == '^': best = go(i - 1, j, k + 1)
-    elif A[i][j] == '>': best = go(i, j + 1, k + 1)
-    elif A[i][j] == 'v': best = go(i + 1, j, k + 1)
-    elif A[i][j] == '<': best = go(i, j - 1, k + 1)
-    else: best = max(go(u, v, k + 1) for u, v in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)])
+    if   A[i][j] == '^': best = go(i - 1, j)
+    elif A[i][j] == '>': best = go(i, j + 1)
+    elif A[i][j] == 'v': best = go(i + 1, j)
+    elif A[i][j] == '<': best = go(i, j - 1)
+    else: best = max(go(u, v) for u, v in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)])
     seen.remove((i, j))
-    return best
+    return 1 + best
 print(f'part 1: {go()}')
 # part 1: 2254
